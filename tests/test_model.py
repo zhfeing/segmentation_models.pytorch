33b19604a6f42bc70d6b8e19c92474710fd24a07
zhfeing <zhf666968@gmail.com>
2020-06-26 11:30:06 +0800

    test

---
+++ tests/test_model.py
@@ -0,0 +1,24 @@
+import torch
+
+import segmentation_models_pytorch as smp
+
+
+# device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+device = torch.device("cpu")
+
+
+for model_name, config in smp.encoders.encoders.items():
+    pretrained_settings = config["pretrained_settings"]
+    for dataset_name in pretrained_settings.keys():
+        print("generating encoder: {} in dataset: {}".format(model_name, dataset_name))
+        model = smp.Unet(
+            encoder_name=model_name,
+            encoder_depth=5,
+            decoder_channels=[256, 128, 64, 32, 16],
+            classes=10,
+            encoder_weights=dataset_name
+        ).to(device)
+
+# x = torch.ones(2, 3, 224, 224).to(device)
+# y = model(x)
+# print(y.shape)
